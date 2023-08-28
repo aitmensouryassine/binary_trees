0e1ce43f41cf44e17838db10a38beb9017c76365
HAJAR <elabdellaoui.hajar@gmail.com>
2023-08-28 16:56:39 +0000

    delete binary tree function

--- binary_trees.h
+++ binary_trees.h
@@ -29,5 +29,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
 
 #endif
