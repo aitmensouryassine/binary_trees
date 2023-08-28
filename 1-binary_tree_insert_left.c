bd5d4bdda2a44901fb310cfdb1208d3193e2cb98
HAJAR <elabdellaoui.hajar@gmail.com>
2023-08-28 16:12:43 +0000

    fix betty check

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,22 +10,22 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new;
+	binary_tree_t *new;
 
-    if (!parent)
-       return (NULL); 
-    
-    new = binary_tree_node(parent, value);
-    if(!new)
-        return (NULL);    
+	if (!parent)
+		return (NULL);
 
-    if (!(parent->left))
-        parent->left = new;
-    else
-    {
-        parent->left->parent = new;
-        new->left = parent->left;
-        parent->left = new;
-    }
-    return (new);
+	new = binary_tree_node(parent, value);
+	if (!new)
+		return (NULL);
+
+	if (!(parent->left))
+		parent->left = new;
+	else
+	{
+		parent->left->parent = new;
+		new->left = parent->left;
+		parent->left = new;
+	}
+	return (new);
 }
