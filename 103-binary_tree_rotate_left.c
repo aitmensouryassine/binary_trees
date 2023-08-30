fb6db78924eb01d11ba3fc6315705f7c62a8b863
EL ABDELLAOUI Hajar <Hajar.ELABDELLAOUI@cegedim.com>
2023-08-30 10:41:57 +0100

    task 103

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,27 +8,37 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	binary_tree_t *new_root, *tmp = NULL;
+	binary_tree_t *new_root, *tmp = NULL, *swap = NULL;
 
 	if (!tree || !(tree->right))
 		return (NULL);
 
+	/* set new root */
 	new_root = tree->right;
-
-	if (new_root->left)
-		tmp = new_root->left;
-
+	/* get left child if exist */
+	tmp = new_root->left;
+	/* set tree to the left of the new root */
 	new_root->left = tree;
-	new_root->parent = NULL;
-	new_root->left->parent = new_root;
-
-	if (new_root->left->left == NULL)
+	/* set tmp the original left to tree right */
+	tree->right = tmp;
+	
+	/* if tmp exists we set its parent to tree */
+	if (tmp)
+		tmp->parent = tree;
+	
+	/* swap the parents of the new and the old root */
+	swap = tree->parent;
+	tree->parent = new_root;
+	new_root->parent = swap;
+	
+	/* if tree had a parent, we set new_root in the correct branch */
+	if (swap)
 	{
-		new_root->left->left = tmp;
-		new_root->left->right = NULL;
+		if (swap->left == tree)
+			swap->left = new_root;
+		else
+			swap->right = new_root;
 	}
-	else
-		new_root->left->right = tmp;
 
 	return (new_root);
 }
