0066c942d047f5ac839fc0c0df9db0826f3f8e8f
HAJAR <elabdellaoui.hajar@gmail.com>
2023-08-31 23:32:53 +0000

    task 34

---
+++ 124-sorted_array_to_avl.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * sorted_array_to_avl - builds an AVL tree from an array
+ * @array: pointer to the first element of the array to be converted
+ * @size: the number of element in the array
+ * Return: pointer to the root node of the created AVL tree,
+ * or NULL on failure
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	if (!array || !size)
+		return (NULL);
+
+	return (array);
+}
