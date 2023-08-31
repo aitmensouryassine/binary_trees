6be7332bed1b5c642fe5001c23455da019fc9032
HAJAR <elabdellaoui.hajar@gmail.com>
2023-08-31 23:11:31 +0000

    task 40

---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * heap_to_sorted_array - converts a Binary Max Heap to a
+ * sorted array of integers
+ * @heap: pointer to the root node of the heap to convert
+ * @size: address to store the size of the array
+ * Return: array must be sorted in descending order
+ */
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+	if (!heap || !size)
+		return (NULL);
+
+	return (heap);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -73,5 +73,12 @@ bst_t *bst_remove(bst_t *root, int value);
 int binary_tree_is_avl(const binary_tree_t *tree);
 avl_t *avl_insert(avl_t **tree, int value);
 int binary_tree_is_heap(const binary_tree_t *tree);
+heap_t *heap_insert(heap_t **root, int value);
+avl_t *array_to_avl(int *array, size_t size);
+avl_t *avl_remove(avl_t *root, int value);
+avl_t *sorted_array_to_avl(int *array, size_t size);
+heap_t *array_to_heap(int *array, size_t size);
+int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
 #endif
